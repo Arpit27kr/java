bb152891534fd2612630ffd54dfb44804362c741
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-02 10:51:49 +0530

    Add files via upload

---
+++ add_two_numbers.java
@@ -0,0 +1,14 @@
+
+public class add_two_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5, num2 = 6,sum;
+		sum = num1 + num2;
+		System.out.println("sum of two numbers is "+sum);
+
+	}
+
+	
+
+}
