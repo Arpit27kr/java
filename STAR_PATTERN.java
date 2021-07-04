d9c300daa3bfe8b70fce6bc9296e473a13fe3f9e
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-04 12:09:17 +0530

    Add files via upload

---
+++ STAR_PATTERN.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class STAR_PATTERN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<6;i++)   // row wala loop
+		{
+			for (int j=0;j<i;j++) //column ka 
+			{
+				System.out.print("*");  //condition true star print 
+			} 
+			
+			System.out.println();  // end line after each row
+		}
+		
+	}
+
+}
+		
+
+	
