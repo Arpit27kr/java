f9aae17027f29c4fc231052e00ae95d5b225d535
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-05 14:11:06 +0530

    Add files via upload

---
+++ Arithmatic_operators.java
@@ -0,0 +1,31 @@
+
+public class Arithmatic_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=25;                     //initialize cariable
+		System.out.println(k);
+		k=k+1;                       //addition
+		System.out.println(k);
+		int p=2;
+		int d=k-p;                 //subtraction
+		System.out.println(d);
+		d=k%p;                     // use modulos
+		System.out.println(d);
+		d=k/p;                     //use '/' in java
+		System.out.println(d);
+		
+		char var='a', sur='b';     //initialize char
+		System.out.println(var);
+		String s1 ="Arpit yadav";   //initialize string
+		String s2="Arnav sharma";
+		System.out.println(s1);
+		System.out.println(s1==s2); 
+		System.out.println(s1 + s2);
+		
+		
+		
+
+	}
+
+}
