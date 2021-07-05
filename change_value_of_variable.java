ee51e4a57e3049269042276abd267da94cbec7ee
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-05 13:44:59 +0530

    Add files via upload

---
+++ change_value_of_variable.java
@@ -0,0 +1,11 @@
+
+public class change_value_of_variable {
+	public static void main(String[] args) {
+	    int k=20;
+	    System.out.println(k); //original value will be printed
+	    k=k+1;
+		System.out.println(k); // updated value will be printed
+	}
+
+}
+
