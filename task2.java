6021fcf1d78f615e41ef7a86da8b3543c3ca53e3
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-05 13:08:17 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,18 @@
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//java program to print even number 1-50
+		for(int i=0;i<50;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}
