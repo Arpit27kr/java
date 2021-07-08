5f2ef411e179a10e5b87f8dc468ff383246d4da1
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-08 15:52:53 +0530

    Add files via upload

---
+++ star_pattern.java
@@ -0,0 +1,20 @@
+//DOWNSIDE UP STAR PATTERN
+public class star_pattern {    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<5;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+
+	}
+
+}
