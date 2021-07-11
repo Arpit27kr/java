5e65790157e4a9c1771b8059d2655528fad7ba3f
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-11 10:35:57 +0530

    Add files via upload

---
+++ Practice2.java
@@ -0,0 +1,31 @@
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=i;j--)	
+				{
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=i;k++)
+			{
+				if(i>1 && k>1)
+				{
+					System.out.print(" ");
+				}
+				else
+				{
+					System.out.print("*");
+				}
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
