bf89b75598d58f72ddc4715f7766ca7fbf0a377f
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-19 01:42:18 +0530

    Add files via upload

---
+++ Oops concept/student.java
@@ -0,0 +1,24 @@
+
+ public class student 
+ {
+
+	int roll;
+	String name;
+ }
+ 
+ class ans{
+	 public static void main(String[] args) {
+	 int roll;
+	 String name;
+	 student s =new student();
+	 s.roll=20;
+	 s.name="john";
+	 System.out.print("Name of  student is " +s.name+ " Roll number is " +s.roll );
+	 
+ 
+	
+ 
+		
+	 }
+}
+
