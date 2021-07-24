156938b6bab2d85ad0d990bb54f7e4f78895af61
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-24 19:38:29 +0530

    Add files via upload

---
+++ Oops concept/method.java
@@ -0,0 +1,21 @@
+
+public class method {
+	
+	static void sum()
+	{
+		int a=12;
+	     int b=15;
+	     int sum;
+	     sum=a+b;
+	     System.out.print(sum);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 sum();
+		
+
+	}
+
+}
