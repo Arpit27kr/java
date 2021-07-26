06fa87d13bea8f50f87910498353425b28f71f8c
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-07-26 07:46:32 +0530

    Add files via upload

---
+++ methodparameter.java
@@ -0,0 +1,19 @@
+
+public class methodparameter { 
+	
+	static void name(String fname, int age)   //information passed to method as variable
+	{
+		
+		
+		System.out.println(fname+ "My age is " +age );
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		name("jacob ", 8);           // it will print jacob
+		name("lily ",16);
+		
+	}
+
+}
