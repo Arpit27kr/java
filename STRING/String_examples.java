2516ba05939fa2168be8fbc4fa232ebdb530bd3d
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-08-08 14:34:43 +0530

    Add files via upload

---
+++ STRING/String_examples.java
@@ -0,0 +1,20 @@
+
+public class String_examples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Arpit";
+		char[]s2= {'a','r','p','i','t'};
+		String s3=new String("Arpit yadav");
+		String s4=new String(s2);
+		System.out.println();
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s4);
+		char s5=s3.charAt(2);     // to print the location of string 
+		System.out.println(s5);
+
+	}
+
+}
