bade8cc76d129726d5347dc85a03824bfdb2670a
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-08-09 17:15:53 +0530

    Add files via upload

---
+++ Experiment Problem/Second_digit_of_number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//Write program to print the second digit. e.g. input 23516 the output is 3
+public class Second_digit_of_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input=new Scanner(System.in);
+		   System.out.print("Enter the number for first number calculation : ");
+			int n=input.nextInt();
+			while(n>=100)
+			{
+				n=n/10;
+				
+				
+			}
+			
+			
+			
+			System.out.println();
+			System.out.print("First number is : " +n);
+			int n2=n%10;
+			System.out.print("First number is : " +n2);
+
+	}
+
+}
