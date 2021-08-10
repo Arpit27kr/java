881175bf0ea1e97765421a2c1f7154131c42e6fc
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-08-10 23:55:24 +0530

    Add files via upload

---
+++ Experiment Problem/POSITIONAL_VALUES.java
@@ -0,0 +1,32 @@
+/*7.	Write program to print positional values of digits. 
+ * Input 21463 output 3, 60, 400, 1000 and 20000.
+ */
+public class POSITIONAL_VALUES {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=61463;
+		
+		
+		
+		int rep=0;
+		for(int i=n;i>0;i--)
+		{
+			for(int k=1;k<n ;k++)
+			{
+				int d=(int)Math.pow(10,k-1);
+			   rep=(n%10)*d;
+			   System.out.println(rep);
+			   n=n/10;
+			   
+			}
+			
+			
+	}
+	
+		
+		
+
+	}
+
+}
