4562b57afa7818f0db18bcb217df86bcde1b689e
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-08-11 00:36:53 +0530

    Update POSITIONAL_VALUES.java

--- Experiment Problem/POSITIONAL_VALUES.java
+++ Experiment Problem/POSITIONAL_VALUES.java
@@ -5,28 +5,41 @@ public class POSITIONAL_VALUES {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=61463;
+		int n=21463;
 		
 		
 		
 		int rep=0;
-		for(int i=n;i>0;i--)
-		{
-			for(int k=1;k<n ;k++)
+		for(int i=n;i>=0;i--)
+	{       int k=1;
+			
+		
+			for( k=1;k<=n ;k++)
 			{
 				int d=(int)Math.pow(10,k-1);
 			   rep=(n%10)*d;
-			   System.out.println(rep);
+			   System.out.print(rep+" , ");
 			   n=n/10;
-			   
+			if(k>n)
+			{
+				
+				   rep=(n%10)*d;
+				   System.out.println(rep);
+				   n=n/10;
+				
+			}
+			   			   
 			}
 			
+		
 			
+			
+									
 	}
 	
 		
 		
 
-	}
+}
 
 }
