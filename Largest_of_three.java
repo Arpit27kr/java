1ae4ddc798ee07977604e1ae7b610505744ab654
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-08-22 23:38:40 +0530

    Add files via upload

---
+++ Largest_of_three.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Largest_of_three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int n1=46;
+		int n2=56;
+		int n3=78;
+		if(n1>n2 && n1>n3) {
+			System.out.print("first number "+n1+" Is greater ");}
+			else if(n2>n3)
+			{
+				System.out.println("second number "+n2+" is greater");	
+			}
+			else
+			{
+				System.out.println("third Number "+n3+" is greater");
+			}
+		
+		
+		
+	}
+
+}
