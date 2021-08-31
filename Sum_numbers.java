df58276f4440dcff57d21ccb0c850c1466e85919
Arpit27kr <86195397+Arpit27kr@users.noreply.github.com>
2021-08-31 19:50:54 +0530

    Add files via upload

---
+++ Sum_numbers.java
@@ -0,0 +1,20 @@
+
+public class Sum_numbers {
+
+	public static void main(String[] args) {
+		int sum=0;
+		// TODO Auto-generated method stub
+		for(int i=90;i<200;i++)
+		{
+			
+			if(i%7==0)
+			{
+				sum=sum+i;
+			}
+			
+		}
+		System.out.println(sum);
+
+	}
+
+}
