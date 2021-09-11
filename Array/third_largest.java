a6f72942be3c5e66c124f4bd89310b7953391f6c
Arpit <arpit27kr@gmail.com>
2021-09-11 22:50:24 +0530

    Add files via upload

---
+++ Array/third_largest.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+public class third_largest{
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner input=new Scanner(System.in);
+     int n=input.nextInt();
+     int [] a=new int [n];
+     for (int i=0;i<n;i++)
+     {
+    	 a[i]=input.nextInt();
+     }
+		
+		int temp=0;
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+		
+		if(a[i]<a[j])		
+		{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+
+		}		           
+		}
+			
+			
+			
+		}	
+		System.out.print("THIRD LARGEST NUMBER IS -------------------->"+a[2]); 
+		
+		
+		
+		
+		
+		
+	}
+
+}
