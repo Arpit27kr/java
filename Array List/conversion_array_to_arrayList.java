3da630f610c88c7db7a10fd415cd008db1aadd66
Arpit <arpit27kr@gmail.com>
2021-09-21 00:29:06 +0530

    Add files via upload

---
+++ Array List/conversion_array_to_arrayList.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class conversion_array_to_arrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] arr = {"Rahul", "Utkarsh",
+                "Shubham", "Neelam"};
+		
+             List list =Arrays.asList(arr);
+             System.out.println(list);
+
+
+		
+
+	}
+
+}
