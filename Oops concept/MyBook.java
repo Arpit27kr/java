de861ecff7dc7178fd0ddd5201ef8b6717f68d50
Arpit <arpit27kr@gmail.com>
2021-09-20 17:00:25 +0530

    Add files via upload

---
+++ Oops concept/MyBook.java
@@ -0,0 +1,16 @@
+package hello_world;
+
+public class MyBook {
+	int x=10;
+	int y=8;
+
+
+
+public static void main (String [] args)
+{
+MyBook myobj1 = new MyBook ();
+          MyBook myobj2 = new MyBook();
+           System.out.println (myobj1.x);
+System.out.println (myobj2.y);
+}
+}
