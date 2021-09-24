f7b86f8dda914e8b3e2c784ae40332be45aa4897
Arpit <arpit27kr@gmail.com>
2021-09-24 09:47:52 +0530

    Update tower_of_hanoi.java

--- recursion/tower_of_hanoi.java
+++ recursion/tower_of_hanoi.java
@@ -20,9 +20,9 @@ public class tower_of_hanoi {
             return;
         }
         
-        toh(n-1,t1id,t3id,t2id);
-        System.out.println(n+"["+t1id+ " -> "+t2id+"]");
-        toh(n-1,t3id,t2id,t1id);
+        toh(n-1,t1id,t3id,t2id);//shifting n-1 (2,1) from tower 1 to tower 3 using tower 2
+        System.out.println(n+"shifting last disc "+t1id+ " -> "+t2id+"]");//shifting last disc(3) from tower 1 to tower 2 using tower 3
+        toh(n-1,t3id,t2id,t1id);//shifting (1,2) disc from tower 3 to tower 2 using tower 1
         
     }
 
