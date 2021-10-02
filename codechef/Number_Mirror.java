1c84a071b75ba024442f6d5348340341b9a4f01c
Arpit <arpit27kr@gmail.com>
2021-10-02 13:13:14 -0700

    Add files via upload

---
+++ codechef/Number_Mirror.java
@@ -0,0 +1,21 @@
+package hello;
+import java.util.*;
+public class Number_Mirror {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		
+		for(int i=0;i<t;i++)
+		{
+			int num=input.nextInt();
+		System.out.println(num);
+		
+		
+		}
+		
+
+	}
+
+}
