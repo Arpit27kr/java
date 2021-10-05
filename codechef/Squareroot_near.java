b907a12679339d49f98a1d987c902bbc04c3f848
Arpit <arpit27kr@gmail.com>
2021-10-05 05:05:05 -0700

    Add files via upload

---
+++ codechef/Squareroot_near.java
@@ -0,0 +1,17 @@
+package hello;
+import java.util.*;
+public class Squareroot_near {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0)
+		{
+		int num=input.nextInt();
+		System.out.println((int)Math.sqrt(num));
+		}		
+
+	}
+
+}
