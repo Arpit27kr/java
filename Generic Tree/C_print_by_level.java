b27949daa05636582c7d3cefa35af4aa5c220fe4
Arpit <arpit27kr@gmail.com>
2021-10-23 01:24:16 -0700

    Add files via upload

---
+++ Generic Tree/C_print_by_level.java
@@ -0,0 +1,108 @@
+package Generic_tree;
+
+import java.util.*;
+
+public class C_print_by_level {
+	
+	private static class Node
+	{
+		int data;
+		 ArrayList<Node>children=new ArrayList();
+	}
+
+	public static void display(Node node)
+	{
+	    String str=node.data+"---->";
+		for(Node child:node.children)
+		{
+			str=str+child.data+",";
+		}
+		
+		System.out.println(str);
+		
+		for(Node child:node.children)
+		{
+			display(child);
+		}
+		
+	}
+	public static int size(Node node)
+	{
+		int s=0;
+		for(Node child:node.children)
+		{
+			int cs=0;
+			 cs=cs+size(child);
+			s=s+cs;
+		}
+		s=s+1;
+	   return s;
+	}
+	public static void level_print(Node node)
+	{
+		Queue<Node>q=new ArrayDeque<>();
+		q.add(node);
+		
+		while(q.size()>0)
+		{
+			node=q.remove();
+			System.out.print(node.data+ " ");
+		for(Node child:node.children)
+		{
+			q.add(child);
+		}
+		
+	}
+		System.out.print(".");
+		
+	}
+
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int []arr= {10,20,30,-1,40,-1,-1,50,60,-1,-1,-1};
+		
+		Node root=null;
+		Stack <Node>st=new Stack<>();
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==-1)
+			{
+				st.pop();
+			}
+			else 
+			{
+				
+					Node t=new Node();
+					t.data=arr[i];
+					if(st.size()>0)
+					{
+					st.peek().children.add(t);
+					
+					}
+					else 
+					{
+						root=t;
+					}
+					st.push(t);
+			}
+		}
+		//display(root);
+		//int s=size(root);
+		//System.out.println("Size of generic tree is ---->"+s);
+		//int max=max(root);
+		//System.out.println("Maximum element in generic tree is ----> " +max);
+		//int height=height(root);
+		//System.out.println("Height of generic tree is ----->" +height);
+		
+	  level_print(root);
+		
+		
+
+		
+	}
+
+}
