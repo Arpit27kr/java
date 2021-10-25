92a2661590cfa1fb2bb30ccc05b7af8a9aff6c08
Arpit <arpit27kr@gmail.com>
2021-10-25 11:20:06 -0700

    Add files via upload

---
+++ STRING/replacestring.java
@@ -0,0 +1,15 @@
+package Generic_tree;
+
+public class replacestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="Hello";
+		System.out.println("old string ----> " +str);
+		String str2="Arpit";
+		str=str.replace(str, str2);
+		System.out.println("new String -----> "+str);
+	}
+
+}
