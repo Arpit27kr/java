04b500be9737cd4ed8e0ec32f4fa44c9a3ecdac7
Arpit <arpit27kr@gmail.com>
2021-11-20 21:58:20 -0800

    Add files via upload

---
+++ Oops concept/wrapper_class.java
@@ -0,0 +1,18 @@
+package revision;
+
+//autoboxing and unboxing
+
+public class wrapper_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5;
+		Integer i=Integer.valueOf(a);
+		Integer j=i;
+		System.out.println(i+" "+j+ " "+a);
+		
+
+	}
+
+}
