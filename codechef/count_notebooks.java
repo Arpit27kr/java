56338d11104a90227f9274efc7de8d2ab849a303
Arpit <arpit27kr@gmail.com>
2022-03-04 03:48:42 -0800

    Add files via upload

---
+++ codechef/count_notebooks.java
@@ -0,0 +1,22 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class count_notebooks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int t=input.nextInt();
+		while(t-->0)
+		{
+			int x=input.nextInt();
+			System.out.println(x*10);
+		}
+		
+		
+		
+
+	}
+
+}
